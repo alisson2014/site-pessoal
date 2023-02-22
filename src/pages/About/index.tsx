70fb5e0d8edc8b1501e1f649e4757342015f60c9
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-02-22 19:57:06 -0300

    [POR-004]feat: desenvolvi a pagina sobre

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,21 @@
+import * as Atoms from "./atoms"
+
 const About = () => {
   return (
-    <div>Sobre...</div>
+    <Atoms.Container>
+      <Atoms.Content>
+        <Atoms.Tittle>Sobre mim</Atoms.Tittle>
+        <Atoms.TextContent>
+          Meu nome é alisson e sou apaixonado por tecnologia, atualmente estou cursando Análise e desenvolvimento de sistemas no Integrado.
+        </Atoms.TextContent>
+        <Atoms.TextContent>
+          Desenvolvo soluções em front end, usando React para web e React Native para mobile.
+        </Atoms.TextContent>
+      </Atoms.Content>
+      <div>
+        A decidir
+      </div>
+    </Atoms.Container>
   );
 };
 
