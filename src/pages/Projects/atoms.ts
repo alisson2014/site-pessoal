449708b9b478fff41008d378eed1837b50e5d711
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-02-23 10:51:20 -0300

    [POR-004]feat: finalizei a page projetos

--- src/pages/Projects/atoms.ts
+++ src/pages/Projects/atoms.ts
@@ -12,8 +12,47 @@ export const Tittle = styled.h2`
   font-weight: 600;
 `
 
-export const BoxContent = styled.div`
+export const BoxContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 90px;
+`
+
+export const Box = styled.div`
+  background-color: whitesmoke;
+  width: 20rem;
+  height: 11rem;
+  border-radius: 1.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  cursor: pointer;
+`
+
+export const BoxText = styled.div`
+  width: 100%;
+  background-color: silver;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 1.3rem;
+  font-weight: 400;
+  padding: 1.12rem 0;
+  transform: rotate(-45deg);
+
+  :hover {
+    letter-spacing: 0.2rem;
+  }
+`
+
+export const Link = styled.a`
+  text-decoration: none;
+  font-size: 1.35rem;
+  font-weight: 400;
+
+  :hover {
+    transition: all 0.8s;
+    color: #f8f8ff;
+    opacity: 0.9;
+  }
 `
