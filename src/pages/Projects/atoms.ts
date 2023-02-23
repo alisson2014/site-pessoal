c17cbaf6b7260f9b15df784871c920fd3293c403
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-02-23 10:32:35 -0300

    [POR-004]feat: criei a page projetos

---
+++ src/pages/Projects/atoms.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 120px;
+`
+
+export const Tittle = styled.h2`
+  font-size: 56px;
+  font-weight: 600;
+`
+
+export const BoxContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 90px;
+`
