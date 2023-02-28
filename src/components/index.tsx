5eb71336f36c74d48f25f0d34b080911f392dd98
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-02-28 09:40:05 -0300

    refactor: organizando as pastas

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,9 @@
 //TODO: Molecules
 import Avatar from "./Molecules/Avatar";
 import ContentMe from "./Molecules/ContentMe";
-import AboutMe from "./Molecules/AboutMe";
-import ProjectsBox from "./Molecules/ProjectsBox";
-import CardBox from "./Molecules/CardBox";
+import AboutMe from "./Organism/AboutMe";
+import ProjectsBox from "./Organism/ProjectsBox";
+import CardBox from "./Organism/CardBox";
 
 //TODO: Template
 import Layout from "./Layout";
