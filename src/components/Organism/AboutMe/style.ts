ade6d17b94a355e118656843b6b1d91f98bf3db0
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-03-03 14:07:23 -0300

    [POR-004]: refatorei o código

--- src/components/Organism/AboutMe/style.ts
+++ src/components/Organism/AboutMe/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const Content = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 14px;
 
   @media (max-width: 520px) {
     align-items: center;
@@ -11,11 +11,11 @@ export const Content = styled.section`
 `
 
 export const TextContent = styled.p`
-  font-size: 24px;
+  font-size: 26px;
   font-weight: 300;
   text-align: justify;
 
   @media (max-width: 520px) {
-    font-size: 22px;
+    font-size: 23px;
   }
 `
