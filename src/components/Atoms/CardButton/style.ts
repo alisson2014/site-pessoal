2d1ec4db53236f040c7a014ea89815ca253dd754
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-03-06 11:45:03 -0300

    [POR-004]: desenvolvi a page projetos

--- src/components/Atoms/CardButton/style.ts
+++ src/components/Atoms/CardButton/style.ts
@@ -1,18 +1,27 @@
 import styled from "styled-components"
 
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 17.5px;
+`
+
 export const SMButton = styled.button`
-  padding: 8px 40px;
-  font-size: 1.15em;
+  padding: 7px 21px;
+  font-size: 1.05em;
   font-weight: 500;
   border: 0;
   border-radius: 12px;
   background-color: whitesmoke;
-  color: #151515;
   cursor: pointer;
   transition: all 0.5s ease-in-out;
 
   &:hover {
     letter-spacing: 2px;
-    transform: scale(1.035);
+    transform: scale(1.01);
+  }
+
+  a {
+    text-decoration: none;
+    color: #151515;
   }
 `
--- src/components/Organism/ProjectsBox/index.tsx
+++ src/components/Organism/ProjectsBox/index.tsx
@@ -1,18 +1,32 @@
-import CardProjects from "../../Atoms/CardProjects";
+import CardButton from "../../Atoms/CardButton";
+import CardProjects from "../../Molecules/CardProjects";
 import * as Atoms from "./atoms";
 
 const ProjectsBox = () => {
   return (
     <Atoms.BoxContainer>
       <CardProjects tittle="Calculator JS" text="
-      Calculadora desenvolvida com HTML, CSS e JavaScript, implementei um theme switch, que permite ter o tema ligth e dark.
-      " />
-      <CardProjects tittle="Calculator JS" text="
-      Calculadora desenvolvida com HTML, CSS e JavaScript, implementei um theme switch, que permite ter o tema ligth e dark.
-      " />
-      <CardProjects tittle="Calculator JS" text="
-      Calculadora desenvolvida com HTML, CSS e JavaScript, implementei um theme switch, que permite ter o tema ligth e dark.
-      " />
+      Calculadora desenvolvida com HTML, CSS e JavaScript, implementei um theme switch, que permite transitar entre o tema ligth e dark.
+      ">
+        <CardButton
+          repoHref="https://github.com/alisson2014/calculator-js"
+          deployHref="https://alisson2014.github.io/calculator-js/"
+        />
+      </CardProjects>
+      <CardProjects tittle="CRUD JS" text="
+      Sistema de cadastro de usuários, podendo criar, ler, editar e deleta-los. Desenvolvido com HTML, Bootstrap e JavaScript.
+      ">
+        <CardButton
+          repoHref="https://github.com/alisson2014/crud-js"
+          deployHref="https://alisson2014.github.io/crud-js/"
+        />
+      </CardProjects>
+      <CardProjects tittle="Brogui PHP" text="
+      Blog desenvolvido no bootcamp catalisa do Integrado, com interação com banco de dados SQL.
+      ">
+        <CardButton
+          repoHref="https://github.com/alisson2014/brogui-bootcamp" />
+      </CardProjects>
     </Atoms.BoxContainer>
   );
 };
