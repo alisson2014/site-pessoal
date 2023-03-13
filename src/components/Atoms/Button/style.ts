1ae34c28b74e7d04d2c5aeba208dd03a67620097
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-03-12 23:05:20 -0300

    style: ajustes no button

--- src/components/Atoms/Button/style.ts
+++ src/components/Atoms/Button/style.ts
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 export const Component = styled.button`
   background-color: ${({ color }) => (color ? color : "#252525")};
-  width: 29vw;
-  height: 9.5vh;
+  width: 396px;
+  height: 81px;
   text-align: center;
   font-size: 2.25em;
   font-weight: 500;
@@ -21,13 +21,19 @@ export const Component = styled.button`
     text-decoration: none;
   }
 
-  @media screen and (max-width: 960px) {
+  @media (min-width: 651px) and (max-width: 1200px) {
     font-size: 1.8em;
-    width: 65vw;
-    height: 10vh;
+    width: 338px;
+    height: 75px;
 
     &:hover {
       transform: scale(1.038);
     }
+  }
+
+  @media (min-width: 250px) and (max-width: 650px) {
+    font-size: 1.68rem;
+    width: 282px;
+    height: 64px;
   }
 `
